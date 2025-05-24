0ef104c6772cbec3c0d95c1ab67e9ff87334b8d2
Ziad Aboalmagd <92919158+ziadwael1812@users.noreply.github.com>
2025-05-24 19:01:35 -0400

    Add Role entity for user-service

---
+++ user-service/src/main/java/com/example/microservice/userservice/entity/Role.java
@@ -0,0 +1,25 @@
+package com.example.microservice.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+}
