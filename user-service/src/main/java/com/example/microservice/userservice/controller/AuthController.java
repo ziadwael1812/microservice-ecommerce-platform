01f34afc528f6b4f800dbf129db1fd3df4d0fcfc
Ziad Aboalmagd <92919158+ziadwael1812@users.noreply.github.com>
2025-05-24 19:03:32 -0400

    Add AuthController for user-service

---
+++ user-service/src/main/java/com/example/microservice/userservice/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.example.microservice.userservice.controller;
+
+import com.example.microservice.userservice.dto.JwtResponse;
+import com.example.microservice.userservice.dto.LoginRequest;
+import com.example.microservice.userservice.dto.MessageResponse;
+import com.example.microservice.userservice.dto.SignupRequest;
+import com.example.microservice.userservice.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        JwtResponse jwtResponse = authService.authenticateUser(loginRequest);
+        return ResponseEntity.ok(jwtResponse);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        try {
+            authService.registerUser(signUpRequest);
+            return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
+        }
+    }
+}
