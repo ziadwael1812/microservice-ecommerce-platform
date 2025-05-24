5fc7e137bb082aca97878570b8c2d8e86cc22dab
Ziad Aboalmagd <92919158+ziadwael1812@users.noreply.github.com>
2025-05-24 19:05:06 -0400

    Add Product entity for product-service

---
+++ product-service/src/main/java/com/example/microservice/productservice/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.microservice.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+}
